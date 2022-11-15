62ec9d879e932019f0cc4be9f59892aa5e7bd4fe
losSanJay <115866740+losSanJay@users.noreply.github.com>
2022-11-15 22:14:45 +0530

    Add files via upload

---
+++ secondTut2/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Locale;
+
+//public class Main {
+
+   // public static void main(String[] args) {
+        /*String greeting = "Hello, World!";
+        int n = greeting.length();
+        System.out.println(greeting.length());
+        String river = "Mississippi";
+        String bigRiver = river.toUpperCase();
+        System.out.println(bigRiver);
+        String river1="Mississippi";
+        String newRiver1=river1.toLowerCase(Locale.ROOT);
+        System.out.println(river1);*/
+        //import java.awt.Rectangle;
+        //public class RectangleTester
+        //{
+            public static void main(String[] args)
+            {
+                Rectangle r1 = new Rectangle(0, 0, 100, 50);
+                Rectangle r2 = new Rectangle(0, 0, 100, 50);
+
+                /* Your code goes here */
+                System.out.println(r1);
+                /* and here */
+            }
+        }
+
+
+        // write your code here
+    }
+}
